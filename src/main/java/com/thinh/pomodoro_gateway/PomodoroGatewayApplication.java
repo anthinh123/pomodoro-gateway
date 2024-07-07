1c66fb22d52f72b8dc2feb0ec3b6620e91d9fa30
thinh <anvanthinh@gmail.com>
2024-07-07 22:07:34 +0700

    init gateway

---
+++ src/main/java/com/thinh/pomodoro_gateway/PomodoroGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.thinh.pomodoro_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoroGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoroGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinh/pomodoro_gateway/PomodoroGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinh.pomodoro_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
