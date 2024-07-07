20c0bf30c96dd83fc970fc4d1538fab98311332b
thinh <anvanthinh@gmail.com>
2024-07-07 22:13:48 +0700

    update config

--- src/main/java/com/thinh/pomodoro_gateway/PomodoroGatewayApplication.java
+++ src/main/java/com/thinh/pomodoro_gateway/PomodoroGatewayApplication.java
@@ -2,8 +2,10 @@ package com.thinh.pomodoro_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PomodoroGatewayApplication {
 
 	public static void main(String[] args) {
