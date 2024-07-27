a9f8563308e59e9a0dc7804716b26779e106004c
thinh <anvanthinh@gmail.com>
2024-07-27 17:34:19 +0700

    validate token at gateway

---
+++ src/main/java/com/thinh/pomodoro_gateway/AuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.thinh.pomodoro_gateway;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthenticationFilter implements GlobalFilter {
+
+    @Value("${security.jwt.token.secret-key}")
+    private String JWT_SECRET;
+
+    @Autowired
+    private RouteValidator validator;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        if (validator.isSecured.test(exchange.getRequest())) {
+            // Extract JWT from Authorization header
+            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            String jwt = authHeader.replace("Bearer ", "");
+            // Validate JWT
+            try {
+                validateToken(jwt);
+            } catch (Exception e) {
+                System.out.println("invalid access...!");
+                throw new RuntimeException("un authorized access to application");
+            }
+        }
+
+        // If valid, proceed to next filter
+        return chain.filter(exchange);
+    }
+
+    private void validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(JWT_SECRET);
+            JWT.require(algorithm)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException("Error while validating token", exception);
+        }
+    }
+}
--- src/main/java/com/thinh/pomodoro_gateway/PomodoroGatewayApplication.java
+++ src/main/java/com/thinh/pomodoro_gateway/PomodoroGatewayApplication.java
@@ -3,13 +3,24 @@ package com.thinh.pomodoro_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@PropertySource("file:${user.dir}/application.env")
 public class PomodoroGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PomodoroGatewayApplication.class, args);
+	}
+
+	@Bean
+	public GlobalFilter authenticationFilter() {
+		return new AuthenticationFilter();
 	}
 
 }
